229c8261f9acd88b52436a6d749acfb8c33e9398
dydwo0740 <80679257+dydwo0740@users.noreply.github.com>
2023-08-10 23:22:53 +0900

    Update stop.sh

--- backend/stop.sh
+++ backend/stop.sh
@@ -5,6 +5,7 @@ TIME_NOW=$(date +%c)
 # 현재 구동 중인 애플리케이션 pid 확인
 CURRENT_PID=$(pgrep -f cicdproject-0.0.1-SNAPSHOT.jar)
 
+
 # 프로세스가 켜져 있으면 종료
 if [ -z $CURRENT_PID ]; then
   echo "$TIME_NOW > 현재 실행중인 애플리케이션이 없습니다"
