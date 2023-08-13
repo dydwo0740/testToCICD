cc2d5b48a42e3f9ba970c497de83e4e996d51b66
이용재 <80679257+dydwo0740@users.noreply.github.com>
2023-08-13 15:36:00 +0900

    Update deploy.sh

--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -1,7 +1,2 @@
 #!/usr/bin/env bash
-
-echo "[$(date)] client deploy" >> /home/ubuntu/deploy.log
-
-sudo cp -rf /home/ubuntu/client/build/*  /home/ubuntu/html/build
-
-sudo systemctl reload apache2
+ echo "> FE 배포"
